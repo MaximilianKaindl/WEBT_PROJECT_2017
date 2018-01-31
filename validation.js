822e5760c9e7a86d4ee10ac12da6f59c3034930e
MaximilianKaindl <31858339+MaximilianKaindl@users.noreply.github.com>
2018-01-31 10:49:35 +0100

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,31 @@
+<script>
+	var qst1 = 0;
+	var qst2 = 0;
+	var qst3 = 0;
+	var qst4 = 0;
+	var qst5 = 0;
+	var qst6 = 0;
+	var qst7 = 0;
+	var qst8 = 0;
+	var qst9= 0;
+	var qst10 = 0;
+	var qst11 = 0;
+	var qst12 = 0;
+	var qst13 = 0;
+	var qst14 = 0;
+	var qst15 = 0;
+	var qst16 = 0;
+	var qst17 = 0;
+	var qst18 = 0;
+	var qst19 = 0;
+	var qst20 = 0;
+	
+	var IsAdicctedToMobilePhone = 0;
+	var IsSozialNetworkFanatic = 0;
+	var IsFeelingSafeWhileSurfing = 0;
+	var IsFeelingUnsageWhileSurfing = 0;
+	
+	function ValidateQst() {
+		
+	}	
+<script>
